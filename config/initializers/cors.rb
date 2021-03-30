e3c4ee0140231cacaaaae22e6a42213e9ad27371
Romy Maghsoudi <romy.mag9@gmail.com>
2021-03-30 07:47:56 -0400

    new CORS origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://monovia-frontend.firebaseapp.com'
+    origins 'https://monovia-a84fb.web.app/'
 
     resource '*',
       headers: :any,
